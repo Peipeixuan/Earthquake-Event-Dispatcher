89bcbf6068f9624cc551ab4592b291a7c8fce3c8
Peixuan <chintan5327@gmail.com>
2025-04-30 19:19:55 +0800

    fix(frontend): fix frontend UNMET DEPENDENCY

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import Simulation from './pages/simulation.jsx';
 import AlertReport from './pages/alert-report.jsx';
 
