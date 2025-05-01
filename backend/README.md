549a579904d997e836ca6486837b330fca2dc706
Ji Kuai <rk427454171@gmail.com>
2025-05-01 21:10:57 +0800

    feat: crawl data from CWA and export to prometheus regularly

---
+++ backend/README.md
@@ -0,0 +1,1 @@
+將 `.env.example` 改名成 `.env` 並放到 `app/.env`，並且更正 `.env` 的內容。
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,13 @@
+
 from typing import Union
 from app.db import check_mysql_connection
+from app.exporter import setup_exporter
+
+from prometheus_client import make_asgi_app
+
 
 from fastapi import FastAPI
+
 
 app = FastAPI()
 
@@ -10,7 +16,13 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/health")
 def health_check():
     db_ok = check_mysql_connection()
     return {"mysql_connected": db_ok}
+
+
+setup_exporter()
+metrics_app = make_asgi_app()
+app.mount("/metrics", metrics_app)
