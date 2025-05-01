e309c29d7c2c2ac651322f8d57557b9e30c70cb2
Ji Kuai <rk427454171@gmail.com>
2025-05-01 21:10:57 +0800

    feat: crawl data from CWA and export to prometheus regularly

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,14 @@
+
 from typing import Union
 from app.db import check_mysql_connection
+from app.exporter import setup_exporter
+
+from prometheus_client import make_asgi_app
+
 
 from fastapi import FastAPI
 from app.routers import earthquake, settings
+
 
 app = FastAPI()
 
@@ -13,7 +19,13 @@ app.include_router(settings.router)
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/health")
 def health_check():
     db_ok = check_mysql_connection()
     return {"mysql_connected": db_ok}
+
+
+setup_exporter()
+metrics_app = make_asgi_app()
+app.mount("/metrics", metrics_app)
